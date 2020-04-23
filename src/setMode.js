28ac3fca5ca8fc57beb766e92a20a32a01596d32
Thomas Kretschmann <t.kretschmann@t-online.de>
2020-04-23 14:56:50 +0200

    now only take changes when true in settings

--- src/setMode.js
+++ src/setMode.js
@@ -4,50 +4,51 @@ const { scripts } = require('./scripts');
 /**
  * @description 
  */
-module.exports = function setMode(hostname, config) {
+module.exports = function setMode(hostname, config){
 	const time = new Date();
 	let appsMode, systemMode;
 
-	const formatTimeDigit = (timeElem) => (timeElem < 10 ? `0${timeElem}` : timeElem);
+	const formatTimeDigit = (timeElem) =>
+
+			timeElem < 10 ? `0${timeElem}` :
+			timeElem;
 	const timeBanner = (time) =>
 		`It's ${formatTimeDigit(time.getHours())}:${formatTimeDigit(time.getMinutes())} - Time to`;
 
 	if (time.getHours() < 7 || time.getHours() >= 20) {
 		console.log(`${timeBanner(time)} take care of your eyes B-)`);
 
-		appsMode = config[hostname].apps ? new PowerShell(scripts.apps.dark) : new PowerShell(scripts.apps.light);
-		systemMode = config[hostname].system
-			? new PowerShell(scripts.system.dark)
-			: new PowerShell(scripts.system.light);
+		if (config[hostname].apps) appsMode = new PowerShell(scripts.apps.dark);
+		if (config[hostname].system) systemMode = new PowerShell(scripts.system.dark);
 	} else {
 		console.log(`${timeBanner(time)} let it shine! ☀`);
 
-		appsMode = config[hostname].apps ? new PowerShell(scripts.apps.light) : new PowerShell(scripts.apps.dark);
-		systemMode = config[hostname].system
-			? new PowerShell(scripts.system.light)
-			: new PowerShell(scripts.system.dark);
+		if (config[hostname].apps) appsMode = new PowerShell(scripts.apps.light);
+		if (config[hostname].system) systemMode = new PowerShell(scripts.system.light);
 	}
 
 	[ appsMode, systemMode ].forEach((field) => {
-		// Handle process errors (e.g. powershell not found)
-		field.on('error', (err) => {
-			console.error(err);
-		});
+		if (field) {
+			// Handle process errors (e.g. powershell not found)
+			field.on('error', (err) => {
+				console.error(err);
+			});
 
-		// // Stdout
-		// field.on('output', (data) => {
-		// 	console.log(data);
-		// });
+			// // Stdout
+			// field.on('output', (data) => {
+			// 	console.log(data);
+			// });
 
-		// Stderr
-		field.on('error-output', (data) => {
-			console.error(data);
-		});
+			// Stderr
+			field.on('error-output', (data) => {
+				console.error(data);
+			});
 
-		// // End
-		// field.on('end', (code) => {
-		// 	// Do Something on end
-		// 	console.log("Script successfully applied.")
-		// });
+			// // End
+			// field.on('end', (code) => {
+			// 	// Do Something on end
+			// 	console.log("Script successfully applied.")
+			// });
+		}
 	});
 };
